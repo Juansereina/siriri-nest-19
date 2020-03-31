1e97fb5555257f36293e3f1fcdd9698e4c9c1da1
Juansereina <juansereina@outlook.com>
2020-03-30 22:29:07 -0500

    New insert mutation
    Signed-off-by: Juansereina <juansereina@outlook.com>

--- src/lib/mutations.js
+++ src/lib/mutations.js
@@ -1,3 +1,15 @@
+import { insertGuest } from './db';
+
 export default {
-  createGuest: (root, args) => {}
+  createGuest: async (root, { guest }) => {
+    try {
+      const { rows } = await insertGuest(guest);
+      const [userCreated] = rows;
+
+      return userCreated;
+    } catch (error) {
+      console.error(error);
+      return 'Something went wrong :(';
+    }
+  }
 };
